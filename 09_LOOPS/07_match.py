5b58859a9923df9c333a824f96e8e7c876cf4736
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-22 01:12:13 +0530

    match

---
+++ 09_LOOPS/07_match.py
@@ -0,0 +1,14 @@
+a = int(input("enter a number : "))
+
+match a :
+    case 5:
+        print("the value is 5 , you won $5")
+    case 10:
+        print("the value is 10 , you won a car")    
+    case _:
+        print("Better luck next time")
+
+
+
+    
+
