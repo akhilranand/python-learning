44ce4ef5babb8fc89daa91874aca63ad96db3679
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-21 07:53:34 +0530

    operators

---
+++ 07_OPERATORS/01_operators.py
@@ -0,0 +1,57 @@
+a=10
+b=12
+
+
+#Arithematic 
+print("Arithematic")
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+print("--------------------------")
+print("Comparison")
+#Comparison
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+print("--------------------------")
+
+#Logical
+print("Logical")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print("---------")
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print("---------")
+
+print(not True)
+print(not False)
+
+print("--------------------------")
+print("Assignment")
+#Assignment
+a = b
+a+=b
+a-b
+a*=b
+a/=b
+a%=b
+a//=b
+
+
