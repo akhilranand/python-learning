95cfa038cd78103e85df01b1670e95b686c1b9ea
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-22 00:58:42 +0530

    Infinite loop

---
+++ 09_LOOPS/03_infiniteLoop.py
@@ -0,0 +1,9 @@
+# DO NOT RUN THIS - Infinite loop example
+# If you run this, it will print forever because 'a' is never incremented
+# To stop it, close the terminal or interrupt the program
+
+# a = 0
+
+# while a < 11:
+#     print(a)
+#     a + 1  
