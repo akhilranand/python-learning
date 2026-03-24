d51411ba7ebbc363e8c35e289d359c85f4329a75
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-24 06:42:11 +0530

    function 1

--- 11_FUNCTIONS/02_functionArgument.py
+++ 11_FUNCTIONS/02_functionArgument.py
@@ -1,4 +1,22 @@
 def sum(a,b,optional=0):
     print("sum is :",a+b+optional)
 
+def mul(a,b):
+    return a*b
+
+
+def data(a,b):
+    print("name is "+a+ " age is " + str(b))
+
+
+def data1(a,b):
+    print("name is "+a+ " age is " + str(b))    
+
 sum(3,8)
+
+product = mul(10,8)
+
+print(product)
+
+data("unni",20)
+data1(b = 23 , a = "manu")
