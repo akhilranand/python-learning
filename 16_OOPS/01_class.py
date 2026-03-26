8686e9e468bdddd46d1a105e2dcbc7c650a9d613
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-27 00:51:44 +0530

    class

---
+++ 16_OOPS/01_class.py
@@ -0,0 +1,16 @@
+class employee:
+    company = "hp"
+
+    def salary(self):
+        return 50000
+    
+
+
+e1 = employee()
+print(e1.salary())  
+print(e1.company)  
+
+
+e2 = employee()
+print(e2.salary())  
+print(e2.company)  
